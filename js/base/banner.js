0cdb51a77bd363b356043f16b05c5cd5c2f1e5a3
Japhics <Jacksoh@163.com>
2017-08-10 01:51:41 +0800

    佳顺：
    1、轮播图信息获取和下拉列表展示信息获取
    2、增加json文件夹，需要ajax获取的信息暂时用json文件模拟
    3、修改首页下拉列表样式

--- js/base/banner.js
+++ js/base/banner.js
@@ -3,68 +3,86 @@
  */
 $(function () {
     $('#logo-car').addClass('slideInLeft');
-    $('#camera_wrap_1').camera({
-        loader:'bar',
-        mobileAutoAdvance: true,
-        barPosition: 'bottom',
-        pagination: false,
-        playPause: false,
-        overlayer: false,
-        navigation:false,
-        loader:false,
-        loaderOpacity: 0,
-        time: 1500,
-        pauseOnClick: false,
-        mobileNavHover: false,
-        onStartLoading: function() {
-            var temp = $('.animate-price');
-            if(temp.hasClass('bounceIn')){
-                temp.removeClass('bounceIn').addClass('bounceOut');
-                setTimeout(function () {
+    var cameraWrap = $('#camera_wrap_1');
+    var loader = $('<div class="camera_loader"/>').appendTo(cameraWrap);
+    $.getJSON("json/loopProduct.json", null, function (data) {//product/rest/looping.action
+        if(!data instanceof Array)
+        return;
+        for (var i = 0; i < data.length; i++) {
+            var item = data[i];
+            cameraWrap.append('<div  data-src="images/' + item.coverpic + '" data-link="xiangqing.html">'
+                + '<div class="camera_caption fadeFromBottom" data-id="' + item.productid + '">' + item.picintroduction + '</div></div>');
+        }
+        var cur = 0;
+        var temp = $('.animate-price');
+        temp.eq(0).text('￥' + data[0].marketprice + '万');
+        temp.eq(1).text('￥' + data[0].sourongprice + '万');
+        loader.remove();
+        cameraWrap.camera({
+            loader: 'bar',
+            mobileAutoAdvance: true,
+            barPosition: 'bottom',
+            pagination: false,
+            playPause: false,
+            overlayer: false,
+            navigation: false,
+            loader: false,
+            loaderOpacity: 0,
+            time: 1500,
+            pauseOnClick: false,
+            mobileNavHover: false,
+            onStartLoading: function () {
+                if (temp.hasClass('bounceIn')) {
+                    temp.removeClass('bounceIn').addClass('bounceOut');
+                    setTimeout(function () {
+                        temp.removeClass('bounceOut').addClass('bounceIn');
+                        var curdata = data[++cur % data.length];
+                        temp.eq(0).text('￥' + curdata.marketprice + '万');
+                        temp.eq(1).text('￥' + curdata.sourongprice + '万');
+                    }, 1000);
+                } else if (temp.hasClass('bounceOut')) {
                     temp.removeClass('bounceOut').addClass('bounceIn');
-                },1000);
-            }else if(temp.hasClass('bounceOut')){
-                temp.removeClass('bounceOut').addClass('bounceIn');
-            }else{
-                temp.removeClass('bounceOut').addClass('bounceIn');
+                } else {
+                    temp.removeClass('bounceOut').addClass('bounceIn');
+                }
+
+            }
+        });
+
+        $(".carlist").click(function () {
+            window.location.href = '/page/searchcarlist.html'
+        });
+
+        var current_scroll_position = 0;
+        $(window).scroll(function () {
+            current_scroll_position = $(this).scrollTop();
+            if (current_scroll_position < 50) {
+                $('#logo-car').addClass('slideInLeft');
+            } else if (current_scroll_position > 50) {
+                $('#logo-car').removeClass('slideInLeft');
             }
 
-        }
-    });
+        });
 
-    $(".carlist").click(function(){
-        window.location.href='/page/searchcarlist.html'
-    });
+        $(".carlistshow").click(function () {
+            $("#carlistshow").css("display", "");
+            $(this).css("display", "none");
+        });
+        $(".carlistoff").click(function () {
+            $("#carlistshow").css("display", "none");
+            $(".carlistshow").css("display", "");
+        });
 
-    var current_scroll_position = 0;
-    $(window).scroll(function () {
-        current_scroll_position = $(this).scrollTop();
-        if(current_scroll_position < 50){
-            $('#logo-car').addClass('slideInLeft');
-        }else if(current_scroll_position > 50){
-            $('#logo-car').removeClass('slideInLeft');
-        }
-
-    });
-
-    $(".carlistshow").click(function () {
-        $("#carlistshow").css("display", "");
-        $(this).css("display", "none");
-    });
-    $(".carlistoff").click(function () {
-        $("#carlistshow").css("display", "none");
-        $(".carlistshow").css("display", "");
-    });
-
-    $('#pic-collect').on('click', function() {
-        var $src = $('#pic-collect').attr('src');
-        if($src === 'images/before_collect.png'){
-            $('#pic-collect').attr('src','images/after-collect.png');
-        }else if($src === 'images/after-collect.png'){
-            $('#pic-collect').attr('src','images/before_collect.png');
-        }
-    });
-
+        $('#pic-collect').on('click', function () {
+            var pc=$('#pic-collect');
+            var $src = pc.attr('src');
+            if ($src === 'images/before_collect.png') {
+                pc.attr('src', 'images/after-collect.png');
+            } else if ($src === 'images/after-collect.png') {
+                pc.attr('src', 'images/before_collect.png');
+            }
+        });
+    })
 
 
 
--- js/base/loadmore.js
+++ js/base/loadmore.js
@@ -2,60 +2,97 @@
  * Created by Administrator on 2017/08/01.
  */
 $(function () {
-    $('#loadmore').on('click',function () {
-       /* $('#load-label img').css({'display':''});
-        $('.get-more').css({'display':'none'});*/
+    function createFrom(data) {
+        var car_type = data.title;
+        var market_price = data.marketprice||'好多';
+        var sourong_price = data.sourongprice||'很少';
+
+        var outer_div = $('<div class="popular-car" style="position: relative"></div>');
+        var link = "window.location.href='xiangqing.html'";
+        var img = $('<img/>').prop('src', data.coverpic&&('images/'+data.coverpic)).attr('onclick', link).css({
+            'width': '100%',
+            'height': '50vw'
+            //'margin-top': '-10vw'
+        });
+
+
+        var inner_div = $('<div></div>').css({
+            'height': '15vw',
+            'position': 'absolute',
+            'bottom': '0',
+            'background-color': 'rgba(0,0,0,0.5)',
+            'color': 'white',
+            'text-align': 'center',
+            'width': '100%'
+        });
+        /*
+         var inner_div = $('<div></div>').css({
+         'height': '15vw',
+         'position': 'relative',
+         'z-index': '1',
+         'top': '-15vw',
+         'background-color': 'rgba(0,0,0,0.5)',
+         'color': 'white',
+         'text-align': 'center',
+         'float': 'left',
+         'width': '100%'
+         });
+        */
+        var inner_div_1 = $('<div class="col-xs-6" style="margin-top: 2vw">' +
+            '<span style="font-size: 8vw;">' + car_type + '</span></div>');
+
+        var inner_div_2 = $('<div class="col-xs-6" style="font-size: 4vw;">' +
+            '<div style="margin-top: 2vw">' +
+            '<span>市价：</span>' +
+            '<span style="text-decoration: line-through;">¥' + market_price + '万</span>' +
+            '</div>' +
+            '<div style="color: orangered">' +
+            '<span>搜融：</span>' +
+            '<span>¥' + sourong_price + '万</span>' +
+            '</div>' +
+            '</div>');
+
+        inner_div.append(inner_div_1);
+        inner_div.append(inner_div_2);
+        outer_div.append(img);
+        outer_div.append(inner_div);
+
+        return outer_div[0];
+    }
+    function loadmore() {
+        /* $('#load-label img').css({'display':''});
+         $('.get-more').css({'display':'none'});*/
         var angle = 0;
         var wheel = setInterval(function() {
-                angle += 6;
+            angle += 6;
             $('#loadmore').css({'transform':'rotate('+ angle +'deg)','transform-origin': '50% 50%'});
             if(angle === 360){
                 angle = 0;
             }
         },10);
 
-        setTimeout(function() {
-             //   $('.get-more').css({'display':'none'});
-                var car_type = '奔驰GLC';
-                var market_price = '150';
-                var sourong_price = '140';
+        $.getJSON('json/displayProduct.json',null,function(data) {//'product/rest/display.action?offset='+carlist.children().length
+            //   $('.get-more').css({'display':'none'});
+            console.log('product/rest/display.action?offset='+carlist.children().length);
+            if(data.end){
+                $('#load-label').remove();
+            }
+            data=data.list;
+            if(!data instanceof Array)
+                return;
+            var f = document.createDocumentFragment();
+            for(var i=0;i<data.length;i++){
+                f.appendChild(createFrom(data[i]));
+            }
+            carlist[0].appendChild(f);
+            //       $('#load-label img').css({'display':'none'});
+            //        $('.get-more').css({'display':'inline-block'});
+            clearInterval(wheel);
+        });
 
-                var outer_div = $('<div class="popular-car"></div>');
-                var img_src = 'images/sliders/slider4.jpg';
-                var link = "window.location.href='xiangqing.html'";
-                var img = $('<img></img>').prop('src',img_src).attr('onclick',link).css({'width': '100%','height': '50vw','margin-top':'-10vw'});
-
-
-                var inner_div = $('<div></div>').css({'height': '15vw','position':'relative','z-index':'1','top':'-15vw','background-color': 'rgba(0,0,0,0.5)',
-                    'color': 'white','text-align':'center','float':'left','width':'100%'});
-
-                var inner_div_1 = $('<div class="col-xs-6" style="margin-top: 2vw">'+
-                    '<span style="font-size: 8vw;">' + car_type + '</span></div>');
-
-                var inner_div_2 = $('<div class="col-xs-6" style="font-size: 4vw;">'+
-                    '<div style="margin-top: 2vw">'+
-                    '<span>市价：</span>'+
-                    '<span style="text-decoration: line-through;">¥' + market_price + '万</span>'+
-                    '</div>'+
-                    '<div style="color: orangered">'+
-                    '<span>搜融：</span>'+
-                    '<span>¥' + sourong_price + '万</span>'+
-                    '</div>'+
-                    '</div>');
-
-                inner_div.append(inner_div_1);
-                inner_div.append(inner_div_2);
-                outer_div.append(img);
-                outer_div.append(inner_div);
-
-               $('#car-list').append(outer_div).append(outer_div).append(outer_div).append(outer_div).append(outer_div);
-
-
-         //       $('#load-label img').css({'display':'none'});
-        //        $('.get-more').css({'display':'inline-block'});
-                clearInterval(wheel);
-        },1000);
-
-    })
+    }
+    var carlist=$('#car-list');
+    $('#loadmore').on('click',loadmore);
+    loadmore();
 
 });
