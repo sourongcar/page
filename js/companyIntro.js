157a9588091ffa95dfc3b88286949a37a917981c
mzcjayli <mzcjayli@163.com>
2017-08-10 23:39:12 +0800

    佳鑫：实现介绍页的接口

--- js/companyIntro.js
+++ js/companyIntro.js
@@ -1,3 +1,25 @@
 /**
  * Created by Franky on 2017/8/7.
  */
+
+
+$(function(){
+
+
+    $.ajax({
+        url:'http://localhost:8080/sourong_car/article/getArticle.action',
+        type:'POST',
+        dataType: "json",
+        data:{
+            'articleid':1
+        },
+        success:function(data){
+
+            $("#articleArea").html(data.content);
+        },
+        error: function () {
+
+        }
+    })
+
+});
--- js/productIntro.js
+++ js/productIntro.js
@@ -1,3 +1,21 @@
 /**
  * Created by Franky on 2017/8/7.
  */
+$(function(){
+
+    $.ajax({
+        url:'http://localhost:8080/sourong_car/article/getArticle.action',
+        type:'POST',
+        dataType: "json",
+        data:{
+            'articleid':2
+        },
+        success:function(data){
+            $("#articleArea").html(data.content);
+        },
+        error: function () {
+
+        }
+    })
+
+});
