0550f8a35653a00b6cffdc6f8503e7c8a568ceab
CrazySzg <15626164246@163.com>
2017-08-12 12:50:14 +0800

    志强：收藏列表部分

--- js/list.js
+++ js/list.js
@@ -5,6 +5,52 @@ $(function(){
     /**
      * 实现公司信息调用
      */
+    var userid = window.localStorage.getItem("userid");
+    var start = 0;
+    var offset = 8;
+    /**
+     * 用户未登录重定向到首页
+     */
+    /*if(userid == null){
+        window.location.href = "index";
+        return;
+    }*/
+    $.ajax({
+        url:'http://localhost:8080/sourong_car/collection/getDisplayList.action',
+        type:'GET',
+        data:{userid:2,start:start,offset:offset},
+        dataType:'JSON',
+        success:function (data) {
+            console.log(data);
+        }
+    });
+    $('#loadmore').on('click',function () {
+        /**
+         * 车轮转动
+         *
+         */
+        var angle = 0;
+        start += offset;
+        var wheel = setInterval(function() {
+            angle += 6;
+            $('#loadmore').css({'transform':'rotate('+ angle +'deg)','transform-origin': '50% 50%'});
+            if(angle === 360){
+                angle = 0;
+            }
+        },10);
+
+
+
+
+
+        /**
+         * 取消车轮转动
+         *
+         */
+        clearInterval(wheel);
+    })
+
+
     $.ajax({
         url:'http://localhost:8080/sourong_car/company/getCompanyinformation.action',
         type:'POST',
@@ -18,5 +64,6 @@ $(function(){
         error: function () {
 
         }
-    })
+    });
+
 });
